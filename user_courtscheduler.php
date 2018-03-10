fdb05d37f7c0e787d335f0daeaa1193d28d7557a
DESKTOP-5FB65M9\Lenovo <reymartg23@yahoo.com>
2018-03-10 09:47:50 +0800

    Added the user_courtscheduler

---
+++ user_courtscheduler.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.7.4
+ */
+
+/**
+ * Database `user_courtscheduler`
+ */
+
+/* `user_courtscheduler`.`schedules` */
+$schedules = array(
+  array('id' => '1','time_from' => '07:00:00','time_to' => '08:00:00','date' => '2018-03-08','name' => 'pits','availability' => '0'),
+  array('id' => '2','time_from' => '08:00:00','time_to' => '09:00:00','date' => '2018-03-08','name' => 'pits','availability' => '0'),
+  array('id' => '3','time_from' => '09:00:00','time_to' => '10:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '4','time_from' => '10:00:00','time_to' => '11:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '5','time_from' => '11:00:00','time_to' => '12:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '6','time_from' => '12:00:00','time_to' => '13:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '7','time_from' => '13:00:00','time_to' => '14:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '8','time_from' => '14:00:00','time_to' => '15:00:00','date' => '0000-00-00','name' => '','availability' => '1'),
+  array('id' => '9','time_from' => '15:00:00','time_to' => '16:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '10','time_from' => '16:00:00','time_to' => '17:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '11','time_from' => '17:00:00','time_to' => '18:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '12','time_from' => '19:00:00','time_to' => '20:00:00','date' => '2018-03-08','name' => '','availability' => '1'),
+  array('id' => '13','time_from' => '20:00:00','time_to' => '21:00:00','date' => '0000-00-00','name' => '','availability' => '1'),
+  array('id' => '14','time_from' => '21:00:00','time_to' => '22:00:00','date' => '2018-03-08','name' => '','availability' => '1')
+);
+
+/* `user_courtscheduler`.`use_tbl` */
+$use_tbl = array(
+);
